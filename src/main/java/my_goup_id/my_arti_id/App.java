1fc79987ed4ff4f1b92a2bdf96b0d589ae6d04c8
brucej@mrc-productivity.com <brucej@mrc-productivity.com>
2019-07-03 15:20:05 -0500

    init

---
+++ src/main/java/my_goup_id/my_arti_id/App.java
@@ -0,0 +1,13 @@
+package my_goup_id.my_arti_id;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
